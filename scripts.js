8aa956c2dbdf55644e67e1e3735ef6ccbff9d103
shubham kumar <shubhofficial1@gmail.com>
2024-07-07 16:06:24 +0530

    Added showMyFeed & stopMyFeed fucntionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const videoEl = document.querySelector("#my-video");
+
 let stream = null;
 
 const constraints = {
@@ -14,6 +16,28 @@ const getMicAndMicrophone = async (e) => {
   }
 };
 
+const showMyFeed = (e) => {
+  console.log("ShowMyFeed is working");
+  videoEl.srcObject = stream;
+  const tracks = stream.getTracks();
+  console.log(tracks);
+};
+
+const stopMyFeed = (e) => {
+  console.log("StopMyFeed is working");
+  const tracks = stream.getTracks();
+  tracks.forEach((track) => {
+    console.log(track);
+    track.stop(); // disassociates the track from the source.
+  });
+};
+
 document
   .querySelector("#share")
   .addEventListener("click", (e) => getMicAndMicrophone(e));
+document
+  .querySelector("#show-video")
+  .addEventListener("click", (e) => showMyFeed(e));
+document
+  .querySelector("#stop-video")
+  .addEventListener("click", (e) => stopMyFeed(e));
