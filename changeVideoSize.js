8867c06c8c732e3beadf71f5574b6784f540eff2
shubham kumar <shubhofficial1@gmail.com>
2024-07-09 20:24:56 +0530

    Added Video Size update feature

--- changeVideoSize.js
+++ changeVideoSize.js
@@ -2,8 +2,27 @@ const supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
 console.log(supportedConstraints);
 
 const changeVideoSize = () => {
-  stream.getTracks().forEach((track) => {
+  //   Contains both audio track & video track
+  //   stream.getTracks().forEach((track) => {
+  //     const capabilities = track.getCapabilities();
+  //     console.log(capabilities);
+  //   });
+
+  stream.getVideoTracks().forEach((track) => {
+    // track is a video track , we can get it's capabilities from .getCapabilities()
+    // or we can apply new constraints with applyConstraints()
     const capabilities = track.getCapabilities();
-    console.log(capabilities);
+    const height = document.querySelector("#vid-height").value;
+    const width = document.querySelector("#vid-width").value;
+    const videoConstraints = {
+      height: {
+        exact:
+          height < capabilities.height.max ? height : capabilities.height.max,
+      },
+      width: {
+        exact: width < capabilities.width.max ? width : capabilities.width.max,
+      },
+    };
+    track.applyConstraints(videoConstraints);
   });
 };
